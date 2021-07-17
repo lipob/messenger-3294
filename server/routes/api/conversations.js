75bed79bfadff8ba9258c339d70c56bdedabd0ab
Leopoldo <leopoldoblanco@gmail.com>
2021-07-17 10:11:38 -0300

    change messages order when get conversations

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -20,9 +20,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      order: [[Message, "createdAt", "ASC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message, order: ["createdAt", "ASC"] },
         {
           model: User,
           as: "user1",
@@ -76,7 +76,8 @@ router.get("/", async (req, res, next) => {
       convoJSON.unreadMessages = unreadMessages.length;
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      const latestMessageIndex = convoJSON.messages.length - 1;
+      convoJSON.latestMessageText = convoJSON.messages[latestMessageIndex].text;
       conversations[i] = convoJSON;
     }
 
